e2e0ea7c88a319920f9c5c311e55eb300472209f
Konstantin <KYuSamsonov@tum.beeline.ru>
2026-05-21 17:04:48 +0300

    SFDM-3663 [Back] gateway авторизация и распределённая логика в HeaderInterceptor

--- src/main/java/ru/beeline/fdmpackloader/config/MyWebMvcConfigurer.java
+++ src/main/java/ru/beeline/fdmpackloader/config/MyWebMvcConfigurer.java
@@ -9,16 +9,9 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import ru.beeline.fdmpackloader.controller.HeaderInterceptor;
 
 @Configuration
 class MyWebMvcConfigurer implements org.springframework.web.servlet.config.annotation.WebMvcConfigurer {
-
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new HeaderInterceptor());
-    }
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
